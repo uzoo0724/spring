aa8b104ddf35da1d785476c0890139cd56f6878b
uzoo0724 <kid07118@naver.com>
2023-02-16 23:30:57 +0900

    chore: 404 error 수정

--- hello/src/main/java/com/springboot/hello/controller/HelloController.java
+++ hello/src/main/java/com/springboot/hello/controller/HelloController.java
@@ -1,4 +1,4 @@
-package controller;
+package com.springboot.hello.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
